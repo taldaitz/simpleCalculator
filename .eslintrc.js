2c0d64449acc763d71e279befe5c39b467b95dd2
Thomas Aldaitz <taldaitz@dawan.fr>
2023-12-19 18:23:26 +0100

    add esLint

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+// .eslintrc.js example
+module.exports = {
+    "env": {
+        "commonjs": true,
+        "jest": true,
+        "browser": true,
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+  }
